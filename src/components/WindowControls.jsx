8a0e1a79cb4aa238788affdf8668d8b01118d9f1
SIDDHI SHARMA <sharmasiddhi2007@gmail.com>
2026-09-19 19:37:42 +0530

    fixed bugs

--- src/components/WindowControls.jsx
+++ src/components/WindowControls.jsx
@@ -1,15 +1,15 @@
 import useWindowStore from "#store/window"
 
 
-const WindowControls = ({target}) => {
-  const {closeWindow} = useWindowStore();
+const WindowControls = ({ target }) => {
+  const { closeWindow } = useWindowStore();
   return (
     <div id="window-controls">
-      <div className="close" onClick={()=> closeWindow(target)}
+      <div className="close" onClick={() => closeWindow(target)}
       />
-      <div className="minimize"/>
-      <div className="maximize"/>
-      
+      <div className="minimize" />
+      <div className="maximize" />
+
     </div>
   )
 }
--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -6,49 +6,49 @@ import { Draggable } from 'gsap/Draggable';
 import React from 'react'
 
 const WindowWrapper = (Component, windowKey) => {
-    const Wrapped = (props) =>{
+    const Wrapped = (props) => {
         const { focusWindow, windows } = useWindowStore();
         const { isOpen, zIndex } = windows[windowKey];
         const ref = useRef(null);
 
-        useGSAP(()=>{
+        useGSAP(() => {
             const el = ref.current;
-            if(!el || !isOpen) return;
+            if (!el || !isOpen) return;
 
             el.style.display = "block";
 
-            gsap.fromTo(el, {scale: 0.8, opacity:0, y:40},{scale:1, opacity:1, y:0, duration: 0.4, ease: "power3.out"},
+            gsap.fromTo(el, { scale: 0.8, opacity: 0, y: 40 }, { scale: 1, opacity: 1, y: 0, duration: 0.4, ease: "power3.out" },
             );
-        },[isOpen]);
+        }, [isOpen]);
 
 
-        useGSAP(()=>{
+        useGSAP(() => {
             const el = ref.current;
-            if(!el) return;
+            if (!el) return;
             const header = el.querySelector("#window-header");
             const [instance] = Draggable.create(el, {
                 trigger: header || el,
                 onPress: () => focusWindow(windowKey)
             });
             return () => instance.kill();
-        },[]);
+        }, []);
 
-        useLayoutEffect(()=>{
+        useLayoutEffect(() => {
             const el = ref.current;
-            if(!el) return;
-            el.style.display = isOpen ? "block":"none";
-        },[isOpen]);
+            if (!el) return;
+            el.style.display = isOpen ? "block" : "none";
+        }, [isOpen]);
 
         return (
-            <section id={windowKey} ref={ref} style={{     zIndex }} className='absolute'>
-            <Component {...props}/>
+            <section id={windowKey} ref={ref} style={{ zIndex }} className='absolute'>
+                <Component {...props} />
             </section>
         );
     };
 
     Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || "Component"})`;
 
-  return Wrapped;
+    return Wrapped;
 }
 
 export default WindowWrapper
